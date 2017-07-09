c543604bdb30930201a1896fc551efabaeda8692
Eric Rinehart <edr27@case.edu>
2017-07-09 18:01:34 -0400

    Encoding of HTTP responses into lazy bytestrings

--- src/Http/Response.hs
+++ src/Http/Response.hs
@@ -1,36 +1,48 @@
 module Http.Response (
     encodeResponse,
     Response(..),
-    StatusCode(..),
     )
 where
 
 import Data.Monoid
 import Data.ByteString.Builder
-import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy as L
 import qualified Data.Map.Strict as Map
 
-encodeResponse :: Response -> B.ByteString
-encodeResponse = toLazyByteString . renderRequest
+encodeResponse :: Response -> L.ByteString
+encodeResponse = toLazyByteString . renderResponse
 
 renderResponse :: Response -> Builder
-renderResponse req =
+renderResponse (Response v c h b) = renderStartingLine v c
+        <> (renderHeaders $ Map.toList h) <> renderBody b
 
--- | HTTP request type
-data StatusCode = 100 | 101 | 200 | 201 | 202 | 203 | 204 | 205 | 206 | 300
-                | 301 | 302 | 303 | 304 | 305 | 306 | 307 | 400 | 401 | 402
-                | 403 | 404 | 405 | 406 | 407 | 408 | 409 | 410 | 411 | 412
-                | 413 | 414 | 415 | 416 | 417 | 500 | 501 | 502 | 503 | 504
-                | 505
+renderStartingLine :: String -> Int -> Builder
+renderStartingLine v c = stringUtf8 "HTTP/" <> stringUtf8 v <> charUtf8 ' '
+        <> renderCodeAndPhrase c
 
-reasonPhrase :: StatusCode -> String
+renderCodeAndPhrase :: Int -> Builder
+renderCodeAndPhrase c = intDec c <> charUtf8 ' ' <> (stringUtf8 $ reasonPhrase c)
+
+renderHeaders :: [(String, String)] -> Builder
+renderHeaders hs = mconcat [ stringUtf8 "\r\n" <> renderHeader h | h <- hs ]
+
+renderHeader :: (String, String) -> Builder
+renderHeader (k, v) = mconcat $ map stringUtf8 [k, ": ", v]
+
+renderBody :: Maybe String -> Builder
+renderBody Nothing = mempty
+renderBody (Just b) = stringUtf8 "\r\n\r\n" <> stringUtf8 b
+
+-- | Reason phrases for status codes
+reasonPhrase :: Int -> String
 reasonPhrase 200 = "OK"
 reasonPhrase 404 = "Not Found"
+reasonPhrase _   = ""
 
 -- | HTTP response data
 data Response = Response
-    { responseVersion :: String,
-      statusCode      :: StatusCode,
+    { httpVersion     :: String,
+      statusCode      :: Int,
       responseHeaders :: Map.Map String String,
       responseBody    :: Maybe String
     } deriving (Show)
--- src/Main.hs
+++ src/Main.hs
@@ -8,10 +8,10 @@ import Http.Response
 
 requestMain = print . parseRequest . pack $ "GET /home HTTP/1.1\nUser-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\nHost: www.example.com\nAccept-Language: en, mi\n\nthis\nis\nthe\nbody"
 responseMain = print . encodeResponse $ Response
-    { responseVersion = "HTTP/1.1",
+    { httpVersion     = "1.1",
       statusCode      = 200,
-      responseHeaders = Map.empty,
-      responseBody    = Nothing
+      responseHeaders = Map.fromList [("User-Agent", "Me"), ("Location", "Here")],
+      responseBody    = Just "This is the\nbody okay\nfriend"
     }
 
 main = responseMain
