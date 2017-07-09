2f48e6f094a961df325bf9a503b2ed014c4887c2
Eric Rinehart <edr27@case.edu>
2017-07-09 11:07:58 -0400

    Added some commenting

--- src/Http/Request.hs
+++ src/Http/Request.hs
@@ -11,6 +11,7 @@ import qualified Data.Attoparsec.ByteString.Char8 as A (takeWhile)
 import qualified Data.ByteString.Char8 as B
 import qualified Data.Map.Strict as Map
 
+-- | Attempt to parse an HTTP request
 parseRequest :: B.ByteString -> Maybe Request
 parseRequest = resultToMaybe . parse requestParser
 
@@ -18,7 +19,6 @@ resultToMaybe :: Result a -> Maybe a
 resultToMaybe (Done _ a) = Just a
 resultToMaybe _          = Nothing
 
--- | Parses a tuple containing the request type, path, HTTP version, headers, and body
 requestParser :: Parser Request
 requestParser = do
         reqType <- typeParser
@@ -28,11 +28,11 @@ requestParser = do
         httpVersion <- versionParser
         endOfLine
         return Request
-            { requestType = reqType,
-              requestPath = reqPath,
+            { requestType    = reqType,
+              requestPath    = reqPath,
               requestVersion = httpVersion,
               requestHeaders = Map.empty,
-              requestBody = Nothing }
+              requestBody    = Nothing }
 
 typeParser :: Parser RequestType
 typeParser = do
@@ -54,7 +54,10 @@ versionParser = do
         remainder <- many ((:) <$> char '.' <*> many1 digit)
         return $ major ++ concat remainder
 
+-- | HTTP request type
 data RequestType = GET | POST deriving (Show, Read)
+
+-- | Parsed HTTP request data
 data Request = Request
     { requestType    :: RequestType,
       requestPath    :: String,
