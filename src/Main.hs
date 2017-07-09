b566ead9dee11390b81605bd72b2f04905c1ac07
Eric Rinehart <edr27@case.edu>
2017-07-09 18:58:01 -0400

    Basic TCP server functionality

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ import Data.ByteString.Char8 (pack)
 import Data.Map as Map
 import Http.Request
 import Http.Response
+import Server
 
 requestMain = print . parseRequest . pack $ "GET /home HTTP/1.1\r\nUser-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\r\nHost: www.example.com\r\nAccept-Language: en, mi\r\n\r\nthis\nis\nthe\nbody"
 responseMain = print . encodeResponse $ Response
@@ -13,5 +14,6 @@ responseMain = print . encodeResponse $ Response
       responseHeaders = Map.fromList [("User-Agent", "Me"), ("Location", "Here")],
       responseBody    = Just "This is the\nbody okay\nfriend"
     }
+serverMain = do runServer "8000"
 
-main = requestMain
+main = serverMain
